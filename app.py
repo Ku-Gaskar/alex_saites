3f754cc44ac93896b1d6978b893c3c5b8d29b1b5
Ku-Gaskar <ku@aver-s.com.ua>
2023-03-14 20:41:12 +0200

    1

--- app.py
+++ app.py
@@ -18,5 +18,5 @@ def index():
     return render_template('index.j2',content=content)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    app.run(host='192.168.1.102', port=8000, debug=True)
  
--- static/js/app_alex.js
+++ static/js/app_alex.js
@@ -1,12 +1,21 @@
 
-var ind=1;
+var ind=0;
 function stopLine(){
-    if (ind==1){
+    if (ind==0){
+      // alert(document.getElementById('tyty').style.animationPlayState);
       document.querySelector('.newsticker span').style.setProperty("--speed_go","10s");
-      ind=0;
+      document.querySelector('span#tyty').style.setProperty('animation-play-state','running');
+      ind++;
     }
     else{
-      document.querySelector('.newsticker span').style.setProperty("--speed_go","");
-      ind=1;
+      if (ind==1){
+        document.querySelector('span#tyty').style.setProperty('animation-play-state','paused');
+       ind++;
+      }
+      else{
+        document.querySelector('.newsticker span').style.setProperty("--speed_go","");
+        ind=0;
+      }
     }
+
 };
