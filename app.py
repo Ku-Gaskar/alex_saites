09fd5ca025e4f8e3ed4383a60f202466a033277a
Ku-Gaskar <ku@aver-s.com.ua>
2023-03-17 16:23:57 +0200

    1

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def index():
     content={}
     content['title'] = '100/100'
 
-    return render_template('index.j2',content=content)
+    return render_template('index.html',content=content)
 
 if __name__ == '__main__':
     app.run(host='192.168.1.102', port=8000, debug=True)
