74e5c653940eacd640c1dbfffead85a0026207df
Ku-Gaskar <ku@aver-s.com.ua>
2023-03-23 22:55:00 +0200

    1

--- DB_SQL_Alchemy.py
+++ DB_SQL_Alchemy.py
@@ -1,25 +1,38 @@
-from flask_sqlalchemy import SQLAlchemy
-from dataclasses import dataclass
-from sqlalchemy_serializer import SerializerMixin
+
+from sqlalchemy import Column, String
+from flask_sqlalchemy import SQLAlchemy,model
+
+# from dataclasses import dataclass
+# from sqlalchemy_serializer import SerializerMixin
 
 
-db=SQLAlchemy()
+db = SQLAlchemy()
 
-
-@dataclass
-class Games(db.Model,SerializerMixin):
-    id_game:int
-    name:str
-    count_people:str
-    time_game:str
+# @dataclass
+class Games(db.Model): #,SerializerMixin):
     __tablename__ = 'games'
+    # id_game varchar NOT NULL,
     id_game = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255),nullable=True)
+	# id_game_group varchar NOT NULL,
+    id_game_group =db.Column(db.String)
+    href_img = db.Column(db.String)
+
+	# name_game varchar NULL,
+    name_game = db.Column(db.String,nullable=True)
+    name_game_ext = db.Column(db.String)
+	# count_people varchar NULL,
     count_people = db.Column(db.String(20))    
+	# time_game interval NULL,
     time_game = db.Column(db.String(10))
-    price:int = db.Column(db.Integer)
-    icon:str = db.Column(db.String)
-    id_game_group:str =db.Column(db.String)
+	# lang varchar NULL,
+    lang = db.Column(db.String)
+	# price numeric NULL,
+    price = db.Column(db.Integer)
+	# status varchar NULL,
+    status = db.Column(db.String)
+	# available bool NULL
+    available= db.Column(db.Boolean)
+
 
     # def as_dict(self):
     #    return {c.name: getattr(self, c.name) for c in self.__table__.columns}
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template,request,Response,jsonify
+from flask import Flask, render_template,request,redirect,url_for
 from psycopg2 import Error
 
 
@@ -8,7 +8,7 @@ from DB_SQL_Alchemy import Games,db
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgress@localhost/Alex_100'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgress@localhost/alex_100'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
@@ -17,16 +17,33 @@ db.init_app(app)
 
 @app.route("/selectJson/<id>",methods=['GET', 'POST'])
 def selectJson(id):
-    data=Games.query.filter(Games.id_game_group == id.split('.')[0]).all()
-    res=';'.join(f"{game.id_game}':'{game.name}" for game in data)
+    data:Games=Games.query.filter(Games.id_game_group == id.split('.')[0]).all()
+    res=';'.join(f"{game.id_game}':'{game.name_game} {game.name_game_ext if game.name_game_ext else ''} " for game in data)
     # a=jsonify(data)
     # for game in data:
     #     res[game.id_game] = game.name
     # res=jsonify(res)
     return res
 
+@app.route("/searchGame", methods=['GET'])
+def searchGame():    
+    content={}
+    content['title'] = '100/100'
+    if request.args.get('search'):
+        textSearch = request.args.get('search')
+        content['info'] = ''
+        try:
+            content['info'] = Games.query.filter(Games.name_game.ilike(f"%{textSearch}%")).order_by(Games.id_game).all()
+        except (Exception,Error) as error:
+            print(f"Ошибка чтения из БД:{error}")
+        
+        return render_template('index.html',content=content)
+    else:
+        return redirect('/')
+
 
 @app.route('/',methods=['GET', 'POST'])
+@app.route('/index',methods=['GET', 'POST'])
 def index():
     
     if request.method== 'GET':
@@ -36,7 +53,7 @@ def index():
     content['title'] = '100/100'
     content['info'] = ''
     try:
-        content['info'] = Games.query.all()
+        content['info'] = Games.query.order_by(Games.id_game).all()
     except (Exception,Error) as error:
         print(f"Ошибка чтения из БД:{error}")
 
